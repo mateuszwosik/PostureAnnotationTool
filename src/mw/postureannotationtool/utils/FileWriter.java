60b8f1d856b8da3476daa166637c64c145a20639
mateuszwosik <mateusz.wosik@gmail.com>
2019-03-20 15:31:58 +0100

    ADD file writer class

---
+++ src/mw/postureannotationtool/utils/FileWriter.java
@@ -0,0 +1,14 @@
+package mw.postureannotationtool.utils;
+
+import mw.postureannotationtool.ui.model.Person;
+
+import java.io.File;
+import java.util.List;
+
+public class FileWriter {
+
+    public static void saveToCSVFile(File file, List<Person> personList) {
+
+    }
+
+}
