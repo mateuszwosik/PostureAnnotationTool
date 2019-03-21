92cd874ccd9893eaabf760007cb4a673e5e5d16d
mateuszwosik <mateusz.wosik@gmail.com>
2019-03-21 15:23:37 +0100

    FIX frame size

--- src/mw/postureannotationtool/ui/view/MainForm.java
+++ src/mw/postureannotationtool/ui/view/MainForm.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class MainForm extends JFrame {
 
     private static final int WIDTH = 800;
-    private static final int HEIGHT = 600;
+    private static final int HEIGHT = 610;
 
     private JPanel mainPanel;
     private JButton openImageButton;
